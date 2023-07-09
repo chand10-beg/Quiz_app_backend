a4601ec3168a9961d6928f3a46088fc8de6ba4d6
Ashu Sharma <ashusharma@Ashus-MacBook-Pro.local>
2023-07-09 10:25:39 +0530

    changes in basic structure

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const url = require('url');
-const app = require('./app');
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, function(){
-  console.log('server running on port 5000');
-});
